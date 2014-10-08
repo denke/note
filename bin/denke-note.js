5c197f43f81c0dc56f7eaa1fa896fdd123915777
Raphael Guarilha <guarilha@gmail.com>
2014-10-08 01:33:09 -0300

    An alpha version of the command-line client.

--- bin/denke-note.js
+++ bin/denke-note.js
@@ -1,21 +1,31 @@
 #!/usr/bin/env node
 
+'use strict';
+
+
+process.title = 'denke-note';
+
 var colors = require('colors'),
-    httpServer = require('../lib/http-server'),
+    note = require('../index.js'),
     portfinder = require('portfinder'),
     opener = require('opener'),
-    argv = require('optimist')
-    .boolean('cors')
-    .argv;
-if (argv.h || argv.help) {
-    console.log([
-        "usage: denke-note [path] [options]",
-        "",
-        "options:",
-        " -p Port to use [8800]",
-        " -a Address to use [0.0.0.0]",
-        "",
-        " -h --help Print this list and exit."
-    ].join('\n'));
-    process.exit();
-}
+    Path = require('path'),
+    parser = require("nomnom");
+
+parser.command('serve')
+	.option('path', {
+      abbr: 'p',
+      help: "The path to the content folder"
+   })
+   .callback(function(opts) {
+      console.log(opts)
+      var config = {};
+      if(opts.path) {
+      	config.content = opts.path;
+      	console.log(config.content);
+      }
+      note.start(config);
+   })
+   .help("serve current folder");
+
+parser.parse();
